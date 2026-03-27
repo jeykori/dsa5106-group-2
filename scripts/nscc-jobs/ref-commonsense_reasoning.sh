daab78704f7efb217236400fe883762b471feede
jeykori <5616230+jeykori@users.noreply.github.com>
2026-03-27 11:44:58 +0800

    chore: update finetune params
    - lower sample_size
    - increase micro_batch_size

--- scripts/nscc-jobs/ref-commonsense_reasoning.sh
+++ scripts/nscc-jobs/ref-commonsense_reasoning.sh
@@ -4,7 +4,7 @@
 #PBS -N DoRA_LLaMA_3.2_3B
 #PBS -q normal
 #PBS -l select=1:ngpus=1
-#PBS -l walltime=2:00:00
+#PBS -l walltime=1:00:00
 #PBS -j oe
 #PBS -o out-run.txt
 
@@ -28,11 +28,12 @@ LORA_ALPHA=32
 CUDA_VISIBLE_DEVICES=0 python finetune.py \
     --base_model "$MODEL" \
     --data_path 'commonsense_170k.json' \
-    --batch_size 16  --micro_batch_size 12 --num_epochs 3 \
+    --batch_size 16  --micro_batch_size 24 --num_epochs 3 \
     --learning_rate 2e-4 --cutoff_len 256 --val_set_size 120 \
     --eval_step 80 --save_step 80  --adapter_name dora \
     --target_modules '["q_proj", "k_proj", "v_proj", "up_proj", "down_proj"]' \
     --lora_r "$LORA_R" --lora_alpha "$LORA_ALPHA" \
+    --sample_size 10000 \
     --use_gradient_checkpointing \
     --matmul_precision "high" \
     --output_dir "$OUT_DIR"
