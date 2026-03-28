af6bc53028e117e71679d61d79a9a402a4ac1bf9
jeykori <5616230+jeykori@users.noreply.github.com>
2026-03-29 01:19:42 +0800

    feat: add evaluate script

---
+++ reproduction/evaluate.py
@@ -0,0 +1,121 @@
+import json
+import os
+import re
+
+import torch
+import transformers
+
+# model_path = "./lora-finetuned"
+model_path = "unsloth/llama-3.2-3b"
+model_name = "unsloth/llama-3.2-3b"
+dataset = "boolq"
+batch_size = 10
+num_beams = 4
+outfile = "./eval_results.json"
+
+def main():
+    # --------------------------------------------------------------------------
+    # Load datasets and model
+    # --------------------------------------------------------------------------
+    dataset_path = f"./datasets/dataset/{dataset}/test.json"
+    if not os.path.exists(dataset_path):
+        raise FileNotFoundError(f"Couldn't find dataset file: {dataset_path}")
+    data = json.load(open(dataset_path, "r"))
+
+    data_batches = []
+
+    for i in range(0, len(data), batch_size):
+        batch = data[i : i + batch_size]
+        data_batches.append(batch)
+
+    model = transformers.AutoModelForCausalLM.from_pretrained(model_path, device_map="auto")
+    model.eval()
+
+    # --------------------------------------------------------------------------
+    # Tokenizer
+    # --------------------------------------------------------------------------
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+    # https://github.com/huggingface/transformers/issues/34842#issuecomment-2527910988
+    # Padding for batch processing
+    tokenizer.padding_side = "left"
+
+    eval_result = []
+    for i, batch in enumerate(data_batches):
+        batch_result = eval_batch(batch, tokenizer, model)
+        eval_result.extend(batch_result)
+        print_result("Batch {i + 1}", batch_result)
+
+        # A bit inefficient, but we just overwrite the latest results
+        with open(outfile, "w", encoding="utf-8") as f:
+            json.dump(eval_result, f, indent=4)
+
+    print_result("Overall score", eval_result)
+
+def generate_prompt(data):
+    if data["input"]:
+        context = "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request."
+    else:
+        context = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
+
+    prompt = [
+        context,
+        "###Instruction:\n" + data["instruction"],
+    ]
+
+    if data["input"]:
+        prompt.append("###Input:\n" + data["input"])
+
+    prompt.append("###Response:\n" + data["output"])
+
+    return "\n\n".join(prompt)
+
+def eval_batch(batch, tokenizer, model):
+    prompts = [generate_prompt({**item, "output": ""}) for item in batch]
+
+    inputs = tokenizer(prompts, return_tensors="pt", padding=True).to(model.device)
+    input_length = inputs.input_ids.shape[1] # All padded, so same length
+
+    with torch.no_grad():
+        gen_outputs = model.generate(
+            **inputs,
+            max_new_tokens=32,
+            num_beams=num_beams,
+            return_dict_in_generate=True,
+            output_scores=True,
+        )
+
+    generated_tokens = gen_outputs.sequences[:, input_length:]
+    outputs = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+
+    batch_result = []
+    for model_output, batch_item in zip(outputs, batch):
+        prediction = extract_answer(model_output)
+        reference = batch_item["answer"]
+
+        result = {
+            **batch_item,
+            "model_output": model_output,
+            "prediction": prediction,
+            "passed": prediction == reference,
+        }
+        batch_result.append(result)
+
+    return batch_result
+
+def print_result(prefix: str, results):
+    score = sum(1 for r in results if r["passed"])
+    total = len(results)
+    accuracy = score / total
+    print(f"{prefix}: {score}/{total} ({accuracy:.2%})")
+
+def extract_answer(model_output):
+    """
+    Only works for boolq for now
+    """
+    predicted_answer = re.findall(r"true|false", model_output.strip().lower())
+    if not predicted_answer:
+        return None
+    return predicted_answer[0]
+
+if __name__ == "__main__":
+    main()
