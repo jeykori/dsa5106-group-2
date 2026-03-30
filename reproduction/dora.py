d1a3a5f7b96d3880ab2b6945f03d97cc847ada94
jeykori <5616230+jeykori@users.noreply.github.com>
2026-03-30 18:16:15 +0800

    fix: freeze all non-dora weights

--- reproduction/dora.py
+++ reproduction/dora.py
@@ -13,7 +13,6 @@ class DoraLayer(torch.nn.Module):
         ):
         super().__init__()
         self.base_layer = base_layer
-        self.base_layer.weight.requires_grad = False # Freeze V
 
         out_features, in_features = base_layer.weight.shape
 
@@ -34,10 +33,9 @@ class DoraLayer(torch.nn.Module):
     def forward(self, x):
         previous_dtype = self.base_layer.weight.dtype
 
-        with torch.no_grad():
-            # delta_V = B @ A * scaling
-            delta_v = (self.lora_B.weight @ self.lora_A.weight) * self.scaling
-            v_new = self.base_layer.weight + delta_v
+        # delta_V = B @ A * scaling
+        delta_v = (self.lora_B.weight @ self.lora_A.weight) * self.scaling
+        v_new = self.base_layer.weight + delta_v
 
         # From reference code: they don't calculate the full m * (V_new / ||V_new||), because this generates a new full tensor in memory
         # norm_scale = m / ||V_new|| (shape: [out_features, 1])
@@ -45,7 +43,7 @@ class DoraLayer(torch.nn.Module):
         norm_scale = self.m / torch.linalg.norm(v_new, dim=1, keepdim=True).detach()
 
         # y = norm_scale * (V @ x + delta_v @ x)
-        base_output = self.base_layer(x) # V @ x
+        base_output = torch.nn.functional.linear(x, self.base_layer.weight) # V @ x
         lora_output = self.lora_B(self.lora_A(x)) * self.scaling  # delta_v @ x
 
         result = norm_scale.view(1, 1, -1) * (base_output + lora_output)
@@ -59,6 +57,11 @@ class DoraLayer(torch.nn.Module):
 
 def inject_dora(model: PreTrainedModel, r: int, lora_alpha: int, target_modules: list[str]):
     print(f"Injecting DoRA into pre-trained model")
+
+    # Freeze all original weights
+    for param in model.parameters():
+            param.requires_grad = False
+
     module_names = list(model.named_modules())
 
     for name, module in module_names:
